2cc6af527013c7a713fe7733625b68d3a20cc742
Kurisu Brooks <kurisubrooks@users.noreply.github.com>
2017-02-15 15:35:32 +1100

    Syntax Fixes

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,7 @@
 const util = require("../utilities");
 
 module.exports = (bot, channel, message) => {
-    message.channel.sendMessage("Ping...")
-    .then((msg) => {
-        msg.edit(`Pong! ${msg.createdTimestamp - message.createdTimestamp}ms`);
-    })
-    .catch((err) => {
-        return util.error(channel, err);
-    });
+    return channel.sendMessage("Ping...")
+        .then(msg => msg.edit(`Pong! ${msg.createdTimestamp - message.createdTimestamp}ms`))
+        .catch(err => util.error(channel, err));
 };
