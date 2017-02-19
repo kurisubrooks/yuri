8f31b8c44204eb2338066da8bcf79807b34ea526
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-19 18:32:19 +1100

    Rename bot object to client

--- commands/eval.js
+++ commands/eval.js
@@ -1,11 +1,11 @@
-module.exports = (bot, channel, message, args) => {
+module.exports = (client, channel, message, args) => {
     let input = `📥\u3000**Input:**\n\`\`\`js\n${args.join(" ")}\n\`\`\``;
-    let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(bot.token, "<Token>")}\n\`\`\``;
+    let error = err => `🚫\u3000**Error:**\n\`\`\`js\n${err.toString().replace(client.token, "<Token>")}\n\`\`\``;
 
     try {
         let output = eval(args.join(" "));
         if (typeof output !== "string") output = require("util").inspect(output, { depth: 1 });
-        let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(bot.token, "<Token>")}\n\`\`\``;
+        let response = `📤\u3000**Output:**\n\`\`\`js\n${output.replace(client.token, "<Token>")}\n\`\`\``;
         if (input.length + response.length > 1900) throw new Error("Output too long!");
         return message.edit(`${input}\n${response}`).catch(err => message.edit(`${input}\n${error(err)}`));
     } catch(err) {
